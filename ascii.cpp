dd7d94721849da75852e678a2a7c3cec76bca2a5
sapnaprane <136076086+sapnaprane@users.noreply.github.com>
2023-06-10 14:38:59 +0530

    Create ascii.cpp
    ascii.cpp

---
+++ ascii.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+  int alph;
+  cout<<"Enter the Number " <<endl; 
+  cin>>alph;
+
+  char ch = alph;
+  cout<<"The ASCII value of the number "<<ch<<endl;
+
+  char asc;
+  cout<<"\n Enter the Character " <<endl; 
+  cin>>asc;
+
+  int ing = asc;
+  cout<<"The ASCII value of the number "<<ing<<endl;
+}
