f7320eb3d055a2fb4d31ce4614eca815111a39ca
sapnaprane <136076086+sapnaprane@users.noreply.github.com>
2023-06-19 21:29:35 +0530

    atozloop.cpp

---
+++ atozloop.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int i=65;
+  char ch;
+  cout<<"\n";
+    
+  while (i<=90)
+    {
+      ch=i;
+    cout<<" "<<ch;
+      i++;
+    }
+  cout<<"\n \n";
+
+  int j=97;
+  char sm;
+  while (j<=122)
+    {
+      sm=j;
+    cout<<" "<<sm;
+      j++;
+    }
+}
